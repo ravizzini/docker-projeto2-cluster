8103e8e2274ff1743967544b2902de08cc59e17b
victor.ravizzini <victor.ravizzini@gmail.com>
2025-07-14 17:45:59 -0300

    alterações

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-0k30rv51y1s6voob93gohy5vof5gqosrd62mvpjrdpxka55veo-81qnuhjlhpxrw4xswixauuw7e 10.10.10.100:2377
